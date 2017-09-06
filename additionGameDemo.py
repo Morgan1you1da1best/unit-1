54abe04bd77cef408359a80dc6d206fc4de36055
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-06 13:34:36 -0400

    Updated from Brython Server: 9/6/2017 1:34:35 PM

--- additionGameDemo.py
+++ additionGameDemo.py
@@ -1,1 +1,8 @@
+#Morgan Baughman
+#8/6/17
+#additionGameDemo.py - First "game"
 
+from random import randint
+num1 = randint(1,10)
+num2 = randint(1,10)
+answer = int(input(str(num1)+ '+' +str(num2)+'='))
