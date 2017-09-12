c7cc31250858e7f63ec282c45327f9b7000683ca
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-11 20:48:30 -0400

    Updated from Brython Server: 9/11/2017 8:48:29 PM

--- slope.py
+++ slope.py
@@ -1,1 +1,11 @@
+#Morgan Baughman
+#9/11/17
+#Slope.py - finding the slope of a line.
 
+x1 = int(input('Enter x1 value: '))
+x2 = int(input('Enter x2 value: '))
+y1 = int(input('Enter y1 value: '))
+y2 = int(input('Enter y2 value: '))
+slope = (y2-y1)/(x2-x1)
+print('The slope is:' , (y2-y1)/(x2-x1))
+print('The equation of the line is: y=' , (slope) , 'x' , y1+slope*x1)
