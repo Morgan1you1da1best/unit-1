156b422d40234fa9506f50c52c29844f4774ba6d
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-12 20:26:15 -0400

    Updated from Brython Server: 9/12/2017 8:26:13 PM

--- Quiz1.py
+++ Quiz1.py
@@ -1,1 +1,10 @@
+#Morgan Baughman
+#8/12/17
+#Quiz 1
 
+print('Morgan Baughman')
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter another number: '))
+print('The sum is: ' , num1 + num2)
+print('The product is: ' , num1*num2)
+print('your lucky number is: ' , (num1*7)+(num2*7))
